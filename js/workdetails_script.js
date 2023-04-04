6eb3e6326ea2835c388e7106368ec3dd1e716e22
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2023-04-04 01:38:11 -0400

    Added works color variations

--- js/workdetails_script.js
+++ js/workdetails_script.js
@@ -19,3 +19,30 @@ $(document).ready(function() {
     }
   });
 });
+
+// Z-index shifts after homescreen
+$(document).ready(function() {
+  // Get the position of the top of the page
+  var video = $('.project-bg');
+  var topPosition = $(window).scrollTop();
+
+  // Check if the user has scrolled away from the top of the page
+  if (topPosition > 0) {
+    // If yes, change the z-index of the box to 10
+    video.css("z-index", -2);
+  }
+
+  // When the user scrolls, check the position of the top of the page again
+  $(window).scroll(function() {
+    var topPosition = $(this).scrollTop();
+
+    // If the user has scrolled away from the top of the page, change the z-index of the box to 10
+    if (topPosition > 0) {
+      video.css("z-index", -2);
+    }
+    // Otherwise, change the z-index of the box back to 1
+    else {
+    video.css("z-index", 9999);
+    }
+  });
+});
